5163ad60cb910292575e26d423c2f24fb407d60d
aurumtell <dayana.astana@yandex.ru>
2022-01-24 15:40:20 +0300

    Lab 1 done.

---
+++ task1-1.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(){
+    int uid = (int)getuid();
+    int gid = (int)getgid();
+    printf("Uid = %d; Gid = %d", uid, gid);
+    return 0;
+}
